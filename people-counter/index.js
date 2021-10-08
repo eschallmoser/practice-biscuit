d19c3311123e9841d80e8368d7845a721c97861b
Erin Schallmoser <dialogofadream@gmail.com>
2021-10-07 21:18:32 -0700

    add more practice with variables and mathematical operations

---
+++ people-counter/index.js
@@ -0,0 +1,14 @@
+// document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+
+// console.log(count);
+
+let myAge = 35
+let humanDogRatio = 7 
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge);
